6099c437d1b7fef214bc17ce4e8c99d657a91650
Iffat Tehseen <iffat.tehseen1109@gmail.com>
2020-02-23 01:42:31 +0500

    app-name added and other modification

--- survey/urls.py
+++ survey/urls.py
@@ -5,10 +5,10 @@ from . import views
 #survey/5/
 #survey/5/result
 #survey/5/vote
-
+app_name = "survey"
 urlpatterns = [
- path('',views.index, name='index'),
- path('<int:question_id>/',views.detail,name = 'detail'),
- path('<int:question_id>/result/',views.result,name='result'),
- path('<int:question_id>/vote/',views.vote,name='vote'),
+                path('',views.index, name='index'),
+                path('<int:question_id>/',views.detail,name = 'detail'),
+                path('<int:question_id>/result/',views.result,name='result'),
+                path('<int:question_id>/vote/',views.vote,name='vote'),
 ]
